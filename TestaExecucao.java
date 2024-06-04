3835a4da4a50bc59d30e1822c1671b6a5b122dd2
Vinícius Silva Rosa <128087730+Hyzaard@users.noreply.github.com>
2024-06-04 16:27:02 -0300

    Update TestaExecucao.java

--- TestaExecucao.java
+++ TestaExecucao.java
@@ -2,17 +2,36 @@ import java.util.Arrays;
 
 public class TestaExecucao {
 
-    public void rodaTeste(int[] vetor, String tipoVetor) {
+    public static void rodaTeste(int[] vetor, String tipoVetor, String ordemVetor, String algoritmo) {
 
-        int[] vetorCopia = Arrays.copyOf(vetor, vetor.length);
-
-        System.out.println(tipoVetor);
-
+        int[] copiaVetor = Arrays.copyOf(vetor, vetor.length);
         MetricaDeOrdenacao metrica = new MetricaDeOrdenacao();
-        OrdenaAlgoritmo.bubbleSort(vetorCopia, metrica);
-        System.out.println("Bubble Sort: " + metrica);
 
-        System.out.println();
+        switch (algoritmo) {
+            case "bubbleSort":
+                OrdenaAlgoritmo.bubbleSort(copiaVetor, metrica);
+                break;
+            case "selectionSort":
+                OrdenaAlgoritmo.selectionSort(copiaVetor, metrica);
+                break;
+            case "insertionSort":
+                OrdenaAlgoritmo.insertionSort(copiaVetor, metrica);
+                break;
+            case "quickSort":
+                OrdenaAlgoritmo.quickSort(copiaVetor, metrica);
+                break;
+            case "mergeSort":
+                OrdenaAlgoritmo.mergeSort(copiaVetor, metrica);
+                break;
+            default:
+                System.out.println("Algoritmo não reconhecido.");
+                return;
+        }
+
+        System.out.printf("Algoritmo: %s | Tamanho do vetor: %d | Tipo: %s (%s)%n",
+                algoritmo, vetor.length, tipoVetor, ordemVetor);
+        System.out.printf("Tempo: %.2f ms | Trocas: %f | Comparações: %f%n%n",
+                metrica.getTempo(), metrica.getTrocas(), metrica.getComparacoes());
     }
 }
 
